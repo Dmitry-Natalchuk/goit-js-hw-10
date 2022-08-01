112eded7ca5e3dacba5c4bb45f52d30592a091f7
Dmitry Natalchuk <next1996@ukr.net>
2022-08-01 16:10:19 +0300

    add start hw 10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+//Бібліотекі
+import Debounce from 'lodash.debounce'
+import { Notify } from 'notiflix/build/notiflix-notify-aio'
+
 import './css/styles.css';
 
+
+const input = document.querySelector("#search-box");
+//Ul
+const countryList = document.querySelector(".country-list");
+//DIV
+const countryInfo = document.querySelector(".country-info");
 const DEBOUNCE_DELAY = 300;
