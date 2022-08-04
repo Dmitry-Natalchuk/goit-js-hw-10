eb0d679eb82d314169ef11ca8d19e95e8770faaa
Dmitry Natalchuk <next1996@ukr.net>
2022-08-04 23:38:17 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //Бібліотекі
 import debounce from 'lodash.debounce'
 import { Notify } from 'notiflix/build/notiflix-notify-aio'
-//====================================================
+
 import './css/styles.css';
 import { fetchCountries } from "./fetchCountries";
 
